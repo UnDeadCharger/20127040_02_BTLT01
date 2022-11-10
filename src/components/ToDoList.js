a7f14b8f3f1178b9e8a2978c1bb19da7e8bebe7d
UnDeadCharger <rexfury121@gmail.com>
2022-11-10 16:47:39 +0700

    Added Code Into Todo, TodoForm, ToDoList

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react'
-import TodoForm from './TodoForm'
-function ToDoList() {
+import React, {useState} from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+function TodoList() {
     const [todos, setTodos] = useState([]);
     
     const addTodo = todo => {
@@ -14,13 +15,42 @@ function ToDoList() {
         setTodos(newTodos)
         console.log(...todos);
     }
-    
+
+    const updateTodo = (todoId, newValue) => {
+        if(!newValue.text || /^\s*$=/.test(newValue.text)){
+            return
+        }
+
+        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)))
+        console.log(...todos);
+    }
+
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id !== id);
+
+        setTodos(removeArr);
+    }
+
+
+    const completeTodo = id =>{
+        let updatedTodos = todos.map(todo =>{
+            if (todo.id === id){
+                todo.isComplete = !todo.isComplete
+            }
+            return todo
+        });
+        setTodos(updatedTodos);
+    };
+
+
     return (
     <div>
         <h1> What's the plan for today</h1>
         <TodoForm onSubmit={addTodo}/>
+        <Todo  todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo}/>
+
     </div>
   )
 }
 
-export default ToDoList
+export default TodoList;
